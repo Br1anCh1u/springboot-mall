fa65737bc52ab1a02b23789f75f775f2648a196c
Br1anCh1u <brianchiu0723@gmail.com>
2022-06-06 17:45:26 +0800

    初始化Springboot project

---
+++ src/test/java/com/br1anch1u/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br1anch1u.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
