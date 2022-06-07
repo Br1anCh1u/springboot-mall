1f3172bfbf0c4555aeb665bc129e5c17c53236ba
Br1anCh1u <brianchiu0723@gmail.com>
2022-06-08 01:06:38 +0800

    帳號功能 - register check email & hide controller response user password

--- src/main/java/com/br1anch1u/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/br1anch1u/springbootmall/service/impl/UserServiceImpl.java
@@ -4,22 +4,38 @@ import com.br1anch1u.springbootmall.dao.UserDao;
 import com.br1anch1u.springbootmall.dto.UserRegisterRequest;
 import com.br1anch1u.springbootmall.model.User;
 import com.br1anch1u.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao dao;
 
     @Override
-    public Integer register(UserRegisterRequest userRegisterRequest) {
-        return dao.createUser(userRegisterRequest);
+    public User getUserById(Integer userId) {
+        return dao.getUserById(userId);
     }
 
     @Override
-    public User getUserById(Integer userId) {
-        return dao.getUserById(userId);
+    public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        //check user id existed
+        User user = dao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null) {
+            logger.warn("{} is used.", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //create user
+        return dao.createUser(userRegisterRequest);
     }
 }
